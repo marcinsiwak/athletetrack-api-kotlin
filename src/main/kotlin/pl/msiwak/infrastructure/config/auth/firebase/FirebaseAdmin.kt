0feb50e10822caea8e543fbe193656544025fe62
msiwak <marcinsiwak15@gmail.com>
2024-09-09 16:31:53 +0200

    setup firebase fix

--- src/main/kotlin/pl/msiwak/infrastructure/config/auth/firebase/FirebaseAdmin.kt
+++ src/main/kotlin/pl/msiwak/infrastructure/config/auth/firebase/FirebaseAdmin.kt
@@ -3,11 +3,13 @@ package pl.msiwak.infrastructure.config.auth.firebase
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
+import java.io.File
 import java.io.InputStream
 
 object FirebaseAdmin {
-    private val serviceAccount: InputStream? =
-        this::class.java.classLoader.getResourceAsStream("sportplatform-b5318-firebase-adminsdk-egpiw-0065c30f75.json")
+    private val serviceAccount: InputStream? = System.getenv("GOOGLE_APPLICATION_CREDENTIALS")?.let {
+        File(it).inputStream()
+    } ?: this::class.java.classLoader.getResourceAsStream("sportplatform-b5318-firebase-adminsdk-egpiw-0065c30f75.json")
     private val options: FirebaseOptions = FirebaseOptions.builder()
         .setCredentials(GoogleCredentials.fromStream(serviceAccount))
         .build()
