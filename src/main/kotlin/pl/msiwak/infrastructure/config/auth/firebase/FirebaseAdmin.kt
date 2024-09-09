7b51bed81da00d1fc96bb5ce873eb2d6161be8ea
msiwak <marcinsiwak15@gmail.com>
2024-09-09 23:51:19 +0200

    setup firebase fix

--- src/main/kotlin/pl/msiwak/infrastructure/config/auth/firebase/FirebaseAdmin.kt
+++ src/main/kotlin/pl/msiwak/infrastructure/config/auth/firebase/FirebaseAdmin.kt
@@ -7,8 +7,9 @@ import java.io.InputStream
 
 object FirebaseAdmin {
     private val serviceAccount: InputStream? =
-        this::class.java.classLoader.getResourceAsStream("sportplatform-b5318-firebase-adminsdk-egpiw-0065c30f75.json")
-            ?: throw IllegalStateException("Firebase service account file not found")
+        System.getenv("FIREBASE_SERVICE_ACCOUNT_KEY")?.byteInputStream()
+            ?: this::class.java.classLoader.getResourceAsStream("sportplatform-b5318-firebase-adminsdk-egpiw-0065c30f75.json")
+            ?: throw IllegalStateException("Firebase service account not found in environment variables or resources")
 
     private val options: FirebaseOptions = FirebaseOptions.builder()
         .setCredentials(GoogleCredentials.fromStream(serviceAccount))
